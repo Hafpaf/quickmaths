bcee17feba84f1c0b8623ca59c45797477afe3ac
hafpaf <haf@hafnium.me>
2018-11-20 11:07:45 +0100

    check_timer to answer working, working on int(float(input())

--- quickmaths.py
+++ quickmaths.py
@@ -8,8 +8,28 @@ import numpy
 import time
 from threading import Timer
 
+#Start timer
 start = time.time()
-input("Write something: ",)
+
+def check_if_number():
+    if int(float(input("Write something: ",))) == True:
+        return check_if_number()
+    else:
+        return "NaN"
+
+#End timer
 end =  time.time()
-rounded_number = round(end-start, 4)
-print(rounded_number, "ms.")
+
+#Round up timer to 3 decimals
+rounded_number = round(end-start, 3)
+print(rounded_number, "ms.") # print output
+
+#Check time taken to answer
+def time_taken_to_answer():
+    if rounded_number > 5:
+        return "Too slow"
+    elif rounded_number < 5:
+        return "Your math skills is pretty fast boi"
+    else:
+        return None
+print(time_taken_to_answer())
