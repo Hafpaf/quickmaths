47507e982ecc75d34627842f6caca62c39cae0a9
hafpaf <haf@hafnium.me>
2018-12-08 21:22:59 +0100

    out of time to implement "run again"

--- quickmaths.py
+++ quickmaths.py
@@ -6,6 +6,7 @@ TODO:
 - Run again
 - Make different kind of questions
 - Print to text file
+- Make it run faster on 2nd try
 '''
 #import math
 import numpy as np
@@ -16,18 +17,25 @@ from random import randrange
 number1 = randrange(1,9,1) #Last digit is step
 number2 = randrange(1,9,1)
 
-type = input("Choose type: + | - | * : ", )
+def choose_type():
+    while True:
+        type = input("Choose type: + | - | * : ", )
+        if type not in ["+","-","*"]:
+            return "try again"
+            return True
+        if type in ["+","-","*"]:
+            return False
 
 input_plus = "+"
 input_minus = "-"
 input_times = "*"
 
 def init_input():
-    if type == "+":
+    if choose_type() == "+":
         return "What is " + str(number1) + " " + str(input_plus) + " " + str(number2) + "?"
-    if type == "-":
+    if choose_type() == "-":
         return "What is " + str(number1) + " " + str(input_minus) + " " + str(number2) + "?"
-    if type == "*":
+    if choose_type() == "*":
         return "What is " + str(number1) + " " + str(input_times) + " " + str(number2) + "?"
 
 #The program calculates the answer
@@ -60,17 +68,17 @@ print("Calculation took", rounded_number, "ms.") # print output
 
 #check answer type and answer
 def check_answer():
-    if type == "+":
+    if choose_type() == "+":
         if number == sum1:
             return time_taken_to_answer()
         else:
             return ("Wrong answer")
-    if type == "-":
+    if choose_type() == "-":
         if number == sum2:
             return time_taken_to_answer()
         else:
             return ("Wrong answer")
-    if type == "*":
+    if choose_type() == "*":
         if number == sum3:
             return time_taken_to_answer()
         else:
@@ -83,9 +91,9 @@ def time_taken_to_answer():
     if rounded_number > 3.5:
         return "Correct, but too slow"
     elif rounded_number < 3.5:
-        return "Your math skill is pretty fast boi"
+        return "Very nice"
     else:
-        return "I got to time_taken_to_answer()"
+        return "I got stopped at time_taken_to_answer()!"
 #        return None
 
 print(check_answer())
@@ -94,15 +102,17 @@ print(" ")
 def run_program_again():
     while True:
         run_again = input("Run again?, y or n: ", )
+        if run_again not in ["y","n"]:
+            return "not valid answer"
+            return True
         if run_again == "y":
-            return input("Choose type: + | - | * : ", )
-            init_input()
-            return ("Calculation took", rounded_number, "ms.")
-            check_answer()
-            time_taken_to_answer()
+            return choose_type()
+            return init_input()
+            return check_answer()
+            return time_taken_to_answer()
         if run_again == "n":
                 return exit()
         else:
-            return "not withing the given answers"
+            return "not within the given answers"
             exit()
 print(run_program_again())
