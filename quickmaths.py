f5b24ab3338a9ce0ebc8d59962cce124b1fb5c96
hafpaf <haf@hafnium.me>
2018-12-10 23:39:19 +0100

    add licens specifications

--- quickmaths.py
+++ quickmaths.py
@@ -1,7 +1,20 @@
 '''
-----------------------------------
-The goal of the game is to calculate FAST!
-----------------------------------
+Quickmaths.py: A simple math trainer
+Copyright (C) 2018  Hafpaf
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 TODO:
 - Run again
 - Make different kind of questions
